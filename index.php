d43f0e8095fbaf3b52c7cbacefa50acbd64a88da
Joel Grissom <joelgrissom5@gmail.com>
2014-12-05 12:13:40 -0700

    removed test files

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// test to see if the user is logged in, if not, redirect to login page
-
-?>
-<h1>TESTING!!!!</h1>
--- prepare.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('inc/conn.inc.php');
-$stmt = $conn->prepare("SELECT * FROM companies WHERE id = ?");
-if($stmt->execute(array($_GET['id']))){
-	while($row = $stmt->fetch()){
-		print_r($row);
-	}
-}
