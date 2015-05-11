9f3beb77e2aa80bb4271f66112ae5636cbb22f91
Joel Grissom <joelgrissom5@gmail.com>
2015-05-10 23:45:30 -0700

    removed unnecessary files

--- inc/footer.inc.php
+++
@@ -1,8 +0,0 @@
-		<footer>
-			<script src="js/bootstrap.min.js"></script>
-			<script src="js/jquery-ui.min.js"></script>
-			<script src="js/mdt.js"></script>
-		</footer>
-	</section>
-</body>
-</html>
